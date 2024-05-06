6e31a7872285511d52a398ac9f3c6c797e045934
bocadillomortadela <146634543+bocadillomortadela@users.noreply.github.com>
2024-05-06 18:18:16 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,173 @@
+const PRODUCTS = [
+  {
+    image: './assets/og-kush.png',
+    name: 'OG Kush',
+    price: '€15 por gramo',
+    tipo: 'Híbrida',
+    stars: 4.5,
+    thc_porcentaje: '15%-20%'
+  },
+  {
+    image: './assets/blue-dream.png',
+    name: 'Blue Dream',
+    price: '€12 por gramo',
+    tipo: 'Sátiva',
+    stars: 4.5,
+    thc_porcentaje: '18%-22%'
+  },
+  {
+    image: './assets/gsc.png',
+    name: 'Girl Scout Cookies',
+    price: '€18 por gramo',
+    tipo: 'Híbrida',
+    stars: 4.5,
+    thc_porcentaje: '20%-25%'
+  },
+  {
+    image: './assets/northern-lights.png',
+    name: 'Northern Lights',
+    price: '€14 por gramo',
+    tipo: 'Índica',
+    stars: 4.5,
+    thc_porcentaje: '15%-20%'
+  },
+  {
+    image: './assets/jack-herer.jpg',
+    name: 'Jack Herer',
+    price: '€16 por gramo',
+    tipo: 'Sátiva',
+    stars: 4.5,
+    thc_porcentaje: '18%-24%'
+  },
+  {
+    image: './assets/white-widow.png',
+    name: 'White Widow',
+    price: '€13 por gramo',
+    tipo: 'Híbrida',
+    stars: 4.5,
+    thc_porcentaje: '18%-25%'
+  },
+  {
+    image: './assets/gg-4.jpg',
+    name: 'Gorilla Glue #4',
+    price: '€20 por gramo',
+    tipo: 'Híbrida',
+    stars: 4.5,
+    thc_porcentaje: '25%-30%'
+  },
+  {
+    image: './assets/purplekush.jpg',
+    name: 'Purple Kush',
+    price: '€17 por gramo',
+    tipo: 'Índica',
+    stars: 3.8,
+    thc_porcentaje: '20%-25%'
+  }
+]
+
+const filterContainer = document.createElement('div')
+filterContainer.id = 'filter'
+
+const filterLabel = document.createElement('label')
+filterLabel.setAttribute('for', 'tipo-select')
+filterLabel.textContent = 'Filtrar por tipo:'
+
+const selectTipo = document.createElement('select')
+selectTipo.id = 'tipo-select'
+
+const optionTodos = document.createElement('option')
+optionTodos.value = 'all'
+optionTodos.textContent = 'Todos'
+
+const optionHibrida = document.createElement('option')
+optionHibrida.value = 'Híbrida'
+optionHibrida.textContent = 'Híbrida'
+
+const optionSativa = document.createElement('option')
+optionSativa.value = 'Sátiva'
+optionSativa.textContent = 'Sátiva'
+
+const optionIndica = document.createElement('option')
+optionIndica.value = 'Índica'
+optionIndica.textContent = 'Índica'
+
+const filterButton = document.createElement('button')
+filterButton.id = 'filter-button'
+filterButton.textContent = 'Filtrar'
+
+const filterReset = document.createElement('button')
+filterReset.id = 'filter-button'
+filterReset.textContent = 'Limpiar Filtros'
+
+selectTipo.appendChild(optionTodos)
+selectTipo.appendChild(optionHibrida)
+selectTipo.appendChild(optionSativa)
+selectTipo.appendChild(optionIndica)
+filterContainer.appendChild(filterLabel)
+filterContainer.appendChild(selectTipo)
+filterContainer.appendChild(filterButton)
+filterContainer.appendChild(filterReset)
+products.appendChild(filterContainer)
+
+const printProducts = (products) => {
+  const productSection = document.querySelector('#products')
+
+  const productsContainer = document.createElement('div')
+  productsContainer.id = 'product-cards'
+  productSection.innerHTML = ''
+  productSection.appendChild(filterContainer)
+  productSection.appendChild(productsContainer)
+
+  for (const product of products) {
+    const divProductContainer = document.createElement('div')
+    const divProduct = document.createElement('div')
+    const imgProduct = document.createElement('img')
+    const typeProduct = document.createElement('p')
+    const nameProduct = document.createElement('h2')
+    const ratingsProduct = document.createElement('p')
+    const divRatingsContainer = document.createElement('div')
+    const priceProduct = document.createElement('p')
+    const buttonProduct = document.createElement('button')
+
+    imgProduct.src = product.image
+    imgProduct.alt = product.name
+    typeProduct.textContent = `Tipo: ${product.tipo}`
+    nameProduct.textContent = product.name
+    priceProduct.textContent = `Precio: ${product.price}`
+    buttonProduct.textContent = 'Comprar'
+
+    ratingsProduct.appendChild(divRatingsContainer)
+    divProduct.appendChild(imgProduct)
+    divProduct.appendChild(typeProduct)
+    divProduct.appendChild(nameProduct)
+    divProduct.appendChild(priceProduct)
+    divProduct.appendChild(ratingsProduct)
+    divProduct.appendChild(buttonProduct)
+    productsContainer.appendChild(divProduct)
+  }
+}
+
+filterReset.addEventListener('click', () => {
+  clearFilters()
+})
+function clearFilters() {
+  selectTipo.value = 'all'
+
+  printProducts(PRODUCTS)
+}
+
+filterButton.addEventListener('click', () => {
+  const selectedType = selectTipo.value
+  let filteredProducts
+
+  if (selectedType === 'all') {
+    filteredProducts = PRODUCTS
+  } else {
+    filteredProducts = PRODUCTS.filter(
+      (product) => product.tipo === selectedType
+    )
+  }
+
+  printProducts(filteredProducts)
+})
+printProducts(PRODUCTS)
